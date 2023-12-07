51544a1ec83ec8510df468c11846bcccd060b2ce
elena <52629966+elenad00@users.noreply.github.com>
2023-12-07 15:39:52 +0000

    Update main.py

--- d6/main.py
+++ d6/main.py
@@ -8,15 +8,13 @@ parent_dir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(parent_dir)
 from aoc_utils import *
 
-## part one ##
-def part_one():
-    return
-
-## part two ##
-def part_two():
-    return
-
-test_input = []
-input = read_file('input.txt')
-part_one()
-part_two()
+times=[58996469]
+records=[478223210191071]
+for i in range(len(times)):
+    c = 0
+    record = records[i]
+    time=times[i]
+    for i in range(time):
+        distance = i*(time-i)
+        if distance>record:c+=1
+    print(c) # 128700, 39594072
