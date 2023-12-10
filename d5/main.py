cb74c2922c119e324b6ab059d8799e1d3b5395f3
elena <elenajeanday@gmail.com>
2023-12-10 01:32:36 +0000

    tidied d5 pt 1

--- d5/main.py
+++ d5/main.py
@@ -21,63 +21,24 @@ def block(block,var):
             return new
     return new
 
-def part_one(run_seeds):
-    s2s = clean_block(input[1])
-    s2f = clean_block(input[2])
-    f2w = clean_block(input[3])
-    w2l = clean_block(input[4])
-    ltt = clean_block(input[5])
-    tth = clean_block(input[6])
-    htl = clean_block(input[7])
-
-    min = 9999999999999999999
-    seed=0
-    for seed in run_seeds:
-        soil=block(s2s,seed)
-        fert=block(s2f,soil)
-        wate=block(f2w,fert)
-        ligh=block(w2l,wate)
-        temp=block(ltt,ligh)
-        humi=block(tth,temp)
-        loca=block(htl,humi)
-        if loca<min:
-            min=loca
-            seed=seed
-    print(min,seed)
-    print('---')
+def part_one():
+    blocks = []
+    for i in range(1,8):
+        blocks.append(clean_block(input[i]))
+    min = seeds[0]
+    for seed in seeds:
+        prev = seed
+        for b in blocks:
+            new = block(b,prev)
+            prev=new
+        if prev<min: min=prev
+    print(min)
     
+def part_two():
+    return
+
 test_input = read_file('test.txt','\n\n')
 input = read_file('input.txt', '\n\n')
-#input=test_input
-seeds = ((input[0].strip('\n')).split(': '))[1]
-seeds = [int(x) for x in seeds.split(' ')]
+seeds = [int(x) for x in (input[0].split(': ')[1].split(' '))]
 
-i=0
-total=len(seeds)
-while i<total:
-    new_seeds = []
-    start = seeds[i]
-    s_range = seeds[i+1]
-    new_seeds=[start+i for i in range(s_range)]
-    i+=2
-    new_seeds.sort()
-    print("finding m loc")
-    part_one(new_seeds) # 261,668,924
-# part_2 - 350,004,378 too high
-
-
-'''
-seeds: 
- 302673608 467797997 
-1787644422 208119536 
- 143576771  99841043 
-4088720102 111819874 
- 946418697  13450451 
-3459931852 262303791 
-2913410855 533641609 
-2178733435  26814354 
-1058342395 175406592
-
-
-2041142901 113138307 
-'''
+part_one() # 261668924
